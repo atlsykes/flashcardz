4e257c1fdf3a4f86de95abd998532bfa67aacbb6
Justin Sykes <atlsykes1@att.net>
2022-01-26 17:50:06 -0500

    updated App.js, created Welcome container

---
+++ src/containers/Welcome/Welcome.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const welcome = () => {
+  return (
+    <div>
+      <h1 class='App-header'>Flashcardz App</h1>
+      <button>Start!</button>
+    </div>
+  )
+}
+
+export default welcome
