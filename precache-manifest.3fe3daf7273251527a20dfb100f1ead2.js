792ff144b5441da13ac47219e8a4a08c39c53827
Justin Sykes <atlsykes1@att.net>
2022-01-28 16:15:16 -0500

    Updates

--- precache-manifest.3fe3daf7273251527a20dfb100f1ead2.js
+++ precache-manifest.3fe3daf7273251527a20dfb100f1ead2.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2b1ebd88efc4c1dd8bb7ef1843bc6238",
+    "revision": "766b715299d56351c43828340acdd4ca",
     "url": "/flashcardz/index.html"
   },
   {
-    "revision": "dc5f95cf794abcbe479a",
-    "url": "/flashcardz/static/css/main.7841df95.chunk.css"
+    "revision": "7ae19a0bdc48770d4661",
+    "url": "/flashcardz/static/css/main.71d779b3.chunk.css"
   },
   {
     "revision": "8b8d5a007a02500c6c63",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/flashcardz/static/js/3.0a738eea.chunk.js"
   },
   {
-    "revision": "dc5f95cf794abcbe479a",
-    "url": "/flashcardz/static/js/main.365be0aa.chunk.js"
+    "revision": "7ae19a0bdc48770d4661",
+    "url": "/flashcardz/static/js/main.0d511434.chunk.js"
   },
   {
     "revision": "f8c67f31da6ffb39f8f4",
