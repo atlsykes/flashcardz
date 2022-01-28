12b1d1347bce80604cfabdebce2a3e9918e457a1
Justin Sykes <atlsykes1@att.net>
2022-01-28 12:06:12 -0500

    updated gitignore to include build folder

---
+++ build/precache-manifest.41e57bb60918d9ccc0739454719f9568.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab4ab7ab162896448476e959b0fe52e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad0605f10b3b46e1669f",
+    "url": "/static/css/main.60a123b0.chunk.css"
+  },
+  {
+    "revision": "8b8d5a007a02500c6c63",
+    "url": "/static/js/2.624f555d.chunk.js"
+  },
+  {
+    "revision": "17cafacefc0671cbaf3e",
+    "url": "/static/js/3.0a738eea.chunk.js"
+  },
+  {
+    "revision": "ad0605f10b3b46e1669f",
+    "url": "/static/js/main.2fcfb6ba.chunk.js"
+  },
+  {
+    "revision": "8911a827080028c65a8f",
+    "url": "/static/js/runtime~main.1d5bc325.js"
+  }
+]);
