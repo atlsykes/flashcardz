97138d11b87fcd1824c1c1cb445f003d188f6dbc
Justin Sykes <atlsykes1@att.net>
2022-01-28 12:20:00 -0500

    Updates

---
+++ precache-manifest.3490af99426afb5a856bc78435096aba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44b636480368b23edf1ce1e469bdc926",
+    "url": "/flashcardz/index.html"
+  },
+  {
+    "revision": "ad0605f10b3b46e1669f",
+    "url": "/flashcardz/static/css/main.60a123b0.chunk.css"
+  },
+  {
+    "revision": "8b8d5a007a02500c6c63",
+    "url": "/flashcardz/static/js/2.624f555d.chunk.js"
+  },
+  {
+    "revision": "17cafacefc0671cbaf3e",
+    "url": "/flashcardz/static/js/3.0a738eea.chunk.js"
+  },
+  {
+    "revision": "ad0605f10b3b46e1669f",
+    "url": "/flashcardz/static/js/main.2fcfb6ba.chunk.js"
+  },
+  {
+    "revision": "f8c67f31da6ffb39f8f4",
+    "url": "/flashcardz/static/js/runtime~main.ad354a5c.js"
+  }
+]);
