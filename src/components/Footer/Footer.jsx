19cf31bc411e31f82d2fe126546ba224c92c8385
Justin Sykes <atlsykes1@att.net>
2025-12-17 10:04:45 -0500

    reinstalled react-scripts after npm audit fix removed, tweaked Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,19 +1,18 @@
-import React from 'react'
-import './Footer.css'
+import "./Footer.css";
 
 const Footer = () => {
   return (
     <div>
-      <footer className='page-footer font-small'>
+      <footer className="page-footer font-small">
         <div
-          className='footer'
-          style={{ textAlign: 'center', marginTop: '10px' }}
+          className="footer"
+          style={{ textAlign: "center", marginTop: "10px" }}
         >
-          <p>Copyright 2022: Justin Sykes</p>
+          <p>Copyright 2025: Justin Sykes</p>
         </div>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
