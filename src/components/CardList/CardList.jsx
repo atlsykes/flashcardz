bc0196d9539b7184f26cee14dd5215bd5d5a5f22
Justin Sykes <atlsykes1@att.net>
2025-12-17 09:44:13 -0500

    updated CardList.jsx, ran npm audit fix --force to remove ALL vulnerabilities

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -1,27 +1,27 @@
-import React from 'react'
-import './CardList.css'
+import "./CardList.css";
 
-const CardList = props => {
-  const list: JSX.Element[] = []
+const CardList = (props) => {
+  const list: JSX.Element[] = [];
   const buildList = () => {
     for (let i = 0; i <= props.cardObj.listAnswer.length - 1; i++) {
       list.push(
-        <li className='list-group-item' key={i}>
+        <li className="list-group-item" key={i}>
           {props.cardObj.listAnswer[i]}
         </li>
-      )
+      );
+      console.log(list);
     }
-  }
+  };
 
-  buildList()
+  buildList();
 
   return (
     <div>
-      <div className='cardList'>
-        <ul className='list-group list-group-flush'>{list}</ul>
+      <div className="cardList">
+        <ul className="list-group list-group-flush">{list}</ul>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default CardList
+export default CardList;
